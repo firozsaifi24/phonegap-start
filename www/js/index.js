36acbb6684fad3ac501fe68cd852e1878860f5ce
firozsaifi24 <firozsaifi24@gmail.com>
2017-05-06 17:04:32 +0530

    commit2

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,4 @@
-// For an introduction to the Blank template, see the following documentation:
+﻿// Fo an introduction to the Blank template, see the following documentation:
 // http://go.microsoft.com/fwlink/?LinkID=397704
 // To debug code on page load in cordova-simulate or on Android devices/emulators: launch your app, set breakpoints, 
 // and then run "window.location.reload()" in the JavaScript Console.
